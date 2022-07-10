6e5b76aceda9de5fc1a3383a940d25ff16991dd3
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-07-10 06:56:37 -0500

    js- retrieve and push data for five day forecast. TODO display to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,13 @@ var citySearch = document.querySelector("#city-search");
 // current weather
 var currentCityWeather = [];
 
+// future forecast
+var dayOneForecast = [];
+var dayTwoForecast = [];
+var dayThreeForecast = [];
+var dayFourForecast = [];
+var dayFiveForecast = [];
+
 // Use city input name to get Lat & Lon coordinates
 var getCity = function (city) {
     var apiLatLonUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=386d421121bbbad42dc1ad82319e7fc0";
@@ -46,7 +53,42 @@ var weatherData = function(cityLat, cityLon) {
                 cityUVI = data.current.uvi;
                 currentCityWeather.push({Temperature, cityTemp}, {Humidity, cityHumidity}, {WindSpeed, cityWind}, {UVI, cityUVI});
                 
-                console.log(currentCityWeather);
+                console.log(currentCityWeather, "Current Weather");
+
+                dayOneTemp = data.daily[0].temp.day;
+                dayOneHumidity = data.daily[0].humidity;
+                dayOneWindSpeed = data.daily[0].wind_speed;
+                dayOneUVI = data.daily[0].uvi;
+                dayOneForecast.push({dayOneTemp}, {dayOneHumidity}, {dayOneWindSpeed}, {dayOneUVI});
+                console.log(dayOneForecast, "Day 1");
+
+                dayTwoTemp = data.daily[1].temp.day;
+                dayTwoHumidity = data.daily[1].humidity;
+                dayTwoWindSpeed = data.daily[1].wind_speed;
+                dayTwoUVI = data.daily[1].uvi;
+                dayTwoForecast.push({dayTwoTemp}, {dayTwoHumidity}, {dayTwoWindSpeed}, {dayTwoUVI});
+                console.log(dayTwoForecast, "Day 2");
+
+                dayThreeTemp = data.daily[2].temp.day;
+                dayThreeHumidity = data.daily[2].humidity;
+                dayThreeWindSpeed = data.daily[2].wind_speed;
+                dayThreeUVI = data.daily[2].uvi;
+                dayThreeForecast.push({dayThreeTemp}, {dayThreeHumidity}, {dayThreeWindSpeed}, {dayThreeUVI});
+                console.log(dayThreeForecast, "Day 3");
+
+                dayFourTemp = data.daily[3].temp.day;
+                dayFourHumidity = data.daily[3].humidity;
+                dayFourWindSpeed = data.daily[3].wind_speed;
+                dayFourUVI = data.daily[3].uvi;
+                dayFourForecast.push({dayFourTemp}, {dayFourHumidity}, {dayFourWindSpeed}, {dayFourUVI});
+                console.log(dayFourForecast, "Day 4");
+
+                dayFiveTemp = data.daily[4].temp.day;
+                dayFiveHumidity = data.daily[4].humidity;
+                dayFiveWindSpeed = data.daily[4].wind_speed;
+                dayFiveUVI = data.daily[4].uvi;
+                dayFiveForecast.push({dayFiveTemp}, {dayFiveHumidity}, {dayFiveWindSpeed}, {dayFiveUVI});
+                console.log(dayFiveForecast, "Day 5");
             })
         }
     })
