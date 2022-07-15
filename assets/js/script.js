95ee5ccc8e61aab4dd3d3c07c3b21cead824e46a
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-07-15 17:35:17 -0500

    all data displaying with icons and color coded uvi. Add save city history, append buttons for recent searches. Look into jquery to make code DRY

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,43 @@ var citySearch = document.querySelector("#city-search");
 
 // current weather
 var weatherEl = document.querySelector("#current-weather");
-
+var currentEl = document.querySelector("#current");
 var cityTempEl = document.querySelector("#Temperature");
 var cityHumidityEl = document.querySelector("#Humidity");
 var cityWindSpeedEl = document.querySelector("#WindSpeed");
 var cityUVIEl = document.querySelector("#UVI");
 var cityUVIBackground = document.querySelector(".UVI");
 
-
 // future forecast
 var dayOneTempForecastEl = document.querySelector("#One-Temperature");
 var dayOneHumidityForecastEl = document.querySelector("#One-Humidity");
 var dayOneWindForecastEl = document.querySelector("#One-Wind");
-var dayTwoForecast = [];
-var dayThreeForecast = [];
-var dayFourForecast = [];
-var dayFiveForecast = [];
+var dayOneDateEl = document.querySelector("#day1");
+var future1El = document.querySelector("#future1");
 
+var dayTwoTempForecastEl = document.querySelector("#Two-Temperature");
+var dayTwoHumidityForecastEl = document.querySelector("#Two-Humidity");
+var dayTWoWindForecastEl = document.querySelector("#Two-Wind");
+var dayTwoDateEl = document.querySelector("#day2");
+var future2El = document.querySelector("#future2");
+
+var dayThreeTempForecastEl = document.querySelector("#Three-Temperature");
+var dayThreeHumidityForecastEl = document.querySelector("#Three-Humidity");
+var dayThreeWindForecastEl = document.querySelector("#Three-Wind");
+var dayThreeDateEl = document.querySelector("#day3");
+var future3El = document.querySelector("#future3");
+
+var dayFourTempForecastEl = document.querySelector("#Four-Temperature");
+var dayFourHumidityForecastEl = document.querySelector("#Four-Humidity");
+var dayFourWindForecastEl = document.querySelector("#Four-Wind");
+var dayFourDateEl = document.querySelector("#day4");
+var future4El = document.querySelector("#future4");
+
+var dayFiveTempForecastEl = document.querySelector("#Five-Temperature");
+var dayFiveHumidityForecastEl = document.querySelector("#Five-Humidity");
+var dayFiveWindForecastEl = document.querySelector("#Five-Wind");
+var dayFiveDateEl = document.querySelector("#day5");
+var future5El = document.querySelector("#future5");
 
 // Use city input name to get Lat & Lon coordinates
 var getCity = function (city) {
@@ -53,9 +73,16 @@ var weatherData = function (cityLat, cityLon) {
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data);
+                // console.log(data.daily[1].dt);
+
+                // weather icon
+                var currentIconEl = document.createElement("img");
+                var currentIcon = data.current.weather[0].icon;
+                currentIconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + currentIcon + "@2x.png");
+                currentEl.appendChild(currentIconEl);
 
                 // cityTemp = data.current.temp;
-                var currentTemp = data.current.temp;
+                var currentTemp = Math.round(data.current.temp);
                 var currentTempEl = document.createElement("span");
                 currentTempEl.textContent = currentTemp;
                 cityTempEl.appendChild(document.createTextNode(currentTemp + "F"));
@@ -68,7 +95,7 @@ var weatherData = function (cityLat, cityLon) {
                 cityHumidityEl.appendChild(document.createTextNode(currentHumidity + "%"));
 
                 // cityWind = data.current.wind_speed;
-                var currentWind = data.current.wind_speed;
+                var currentWind = Math.round(data.current.wind_speed);
                 var currentWindEl = document.createElement("span");
                 currentWindEl.textContent = currentWind;
                 cityWindSpeedEl.appendChild(document.createTextNode(currentWind + "MPH"));
@@ -83,55 +110,137 @@ var weatherData = function (cityLat, cityLon) {
                 if (currentUVI <= 2) {
                     cityUVIBackground.style.background = "green";
                 } else if (currentUVI <= 5) {
-                    cityUVIBackground.style.background = "yellow";
+                    cityUVIBackground.style.background = "goldenrod";
                 } else {
                     cityUVIBackground.style.background = "red";
                 };
 
                 // forecast data
-                var dayOneTemp = data.daily[0].temp.day;
+                var future1Icon = data.daily[1].weather[0].icon;
+                var future1IconEl = document.createElement("img")
+                future1IconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + future1Icon + "@2x.png");
+                future1El.appendChild(future1IconEl);
+
+                var dayOneTemp = Math.round(data.daily[1].temp.day);
                 var dayOneTempEl = document.createElement("span");
                 dayOneTempEl.textContent = dayOneTemp;
                 dayOneTempForecastEl.appendChild(document.createTextNode(dayOneTemp + "F"));
 
-                var dayOneHumidity = data.daily[0].humidity;
+                var dayOneHumidity = data.daily[1].humidity;
                 var dayOneHumidityEl = document.createElement("span");
                 dayOneHumidityEl.textContent = dayOneHumidity;
                 dayOneHumidityForecastEl.appendChild(document.createTextNode(dayOneHumidity + "%"));
 
-                var dayOneWindSpeed = data.daily[0].wind_speed;
+                var dayOneWindSpeed = Math.round(data.daily[1].wind_speed);
                 var dayOneWindEl = document.createElement("span");
                 dayOneWindEl.textContent = dayOneWindSpeed;
                 dayOneWindForecastEl.appendChild(document.createTextNode(dayOneWindSpeed + "MPH"));
 
+                var dailyDate1 = moment.unix(data.daily[1].dt).format("dd MM DD YYYY");
+                // console.log(dailyDate);
+                var dailyDate1El = document.createElement("span");
+                dailyDate1El.textContent = dailyDate1;
+                dayOneDateEl.appendChild(document.createTextNode(dailyDate1));
 
-                dayTwoTemp = data.daily[1].temp.day;
-                dayTwoHumidity = data.daily[1].humidity;
-                dayTwoWindSpeed = data.daily[1].wind_speed;
-                dayTwoUVI = data.daily[1].uvi;
-                dayTwoForecast.push({ dayTwoTemp }, { dayTwoHumidity }, { dayTwoWindSpeed }, { dayTwoUVI });
-                console.log(dayTwoForecast, "Day 2");
+                var future2Icon = data.daily[2].weather[0].icon;
+                var future2IconEl = document.createElement("img");
+                future2IconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + future2Icon + "@2x.png");
+                future2El.appendChild(future2IconEl);
 
-                dayThreeTemp = data.daily[2].temp.day;
-                dayThreeHumidity = data.daily[2].humidity;
-                dayThreeWindSpeed = data.daily[2].wind_speed;
-                dayThreeUVI = data.daily[2].uvi;
-                dayThreeForecast.push({ dayThreeTemp }, { dayThreeHumidity }, { dayThreeWindSpeed }, { dayThreeUVI });
-                console.log(dayThreeForecast, "Day 3");
+                var dayTwoTemp = Math.round(data.daily[2].temp.day);
+                var dayTwoTempEl = document.createElement("span");
+                dayTwoTempEl.textContent = dayTwoTemp;
+                dayTwoTempForecastEl.appendChild(document.createTextNode(dayTwoTemp + "F"));
 
-                dayFourTemp = data.daily[3].temp.day;
-                dayFourHumidity = data.daily[3].humidity;
-                dayFourWindSpeed = data.daily[3].wind_speed;
-                dayFourUVI = data.daily[3].uvi;
-                dayFourForecast.push({ dayFourTemp }, { dayFourHumidity }, { dayFourWindSpeed }, { dayFourUVI });
-                console.log(dayFourForecast, "Day 4");
+                var dayTwoHumidity = data.daily[2].humidity;
+                var dayTwoHumidityEl = document.createElement("span");
+                dayTwoHumidityEl.textContent = dayTwoHumidity;
+                dayTwoHumidityForecastEl.appendChild(document.createTextNode(dayTwoHumidity + "%"));
+                
+                var dayTwoWindSpeed = Math.round(data.daily[2].wind_speed);
+                var dayTwoWindSpeedEl = document.createElement("span");
+                dayTwoWindSpeedEl.textContent = dayTwoWindSpeed;
+                dayTWoWindForecastEl.appendChild(document.createTextNode(dayTwoWindSpeed + "MPH"));
 
-                dayFiveTemp = data.daily[4].temp.day;
-                dayFiveHumidity = data.daily[4].humidity;
-                dayFiveWindSpeed = data.daily[4].wind_speed;
-                dayFiveUVI = data.daily[4].uvi;
-                dayFiveForecast.push({ dayFiveTemp }, { dayFiveHumidity }, { dayFiveWindSpeed }, { dayFiveUVI });
-                console.log(dayFiveForecast, "Day 5");
+                var dailyDate2 = moment.unix(data.daily[2].dt).format("dd MM DD YYYY");
+                var dailyDate2El = document.createElement("span");
+                dailyDate2El.textContent = dailyDate2;
+                dayTwoDateEl.appendChild(document.createTextNode(dailyDate2));
+                
+                var future3Icon = data.daily[3].weather[0].icon;
+                var future3IconEl = document.createElement("img");
+                future3IconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + future3Icon + "@2x.png");
+                future3El.appendChild(future3IconEl);
+
+                var dayThreeTemp = Math.round(data.daily[3].temp.day);
+                var dayThreeTempEl = document.createElement("span");
+                dayThreeTempEl.textContent = dayThreeTemp;
+                dayThreeTempForecastEl.appendChild(document.createTextNode(dayThreeTemp + "F"));
+                
+                var dayThreeHumidity = data.daily[3].humidity;
+                var dayThreeHumidityEl = document.createElement("span");
+                dayThreeHumidityEl.textContent = dayThreeHumidity;
+                dayThreeHumidityForecastEl.appendChild(document.createTextNode(dayThreeHumidity + "%"));
+                
+                var dayThreeWindSpeed = Math.round(data.daily[3].wind_speed);
+                var dayThreeWindSpeedEl = document.createElement("span");
+                dayThreeWindSpeedEl.textContent = dayThreeWindSpeed;
+                dayThreeWindForecastEl.appendChild(document.createTextNode(dayThreeWindSpeed + "MPH"));
+
+                var dailyDate3 = moment.unix(data.daily[3].dt).format("dd MM DD YYYY");
+                var dailyDate3El = document.createElement("span");
+                dailyDate3El.textContent = dailyDate3;
+                dayThreeDateEl.appendChild(document.createTextNode(dailyDate3));
+
+                var future4Icon = data.daily[4].weather[0].icon
+                var future4IconEl = document.createElement("img");
+                future4IconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + future4Icon + "@2x.png");
+                future4El.appendChild(future4IconEl);
+
+                var dayFourTemp = Math.round(data.daily[4].temp.day);
+                var dayFourTempEl = document.createElement("span");
+                dayFourTempEl.textContent = dayFourTemp;
+                dayFourTempForecastEl.appendChild(document.createTextNode(dayFourTemp + "F"));
+
+                var dayFourHumidity = data.daily[4].humidity;
+                var dayFourHumidityEl = document.createElement("span");
+                dayFourHumidityEl.textContent = dayFourHumidity;
+                dayFourHumidityForecastEl.appendChild(document.createTextNode(dayFourHumidity + "%"));
+
+                var dayFourWindSpeed = Math.round(data.daily[4].wind_speed);
+                var dayFourWindSpeedEl = document.createElement("span");
+                dayFourWindSpeedEl.textContent = dayFourWindSpeed;
+                dayFourWindForecastEl.appendChild(document.createTextNode(dayFourWindSpeed + "MPH"));
+
+                var dailyDate4 = moment.unix(data.daily[4].dt).format("dd MM DD YYYY");
+                var dailyDate4El = document.createElement("span");
+                dailyDate4El.textContent = dailyDate4;
+                dayFourDateEl.appendChild(document.createTextNode(dailyDate4));
+                
+                var future5Icon = data.daily[5].weather[0].icon;
+                var future5IconEl = document.createElement("img");
+                future5IconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + future5Icon + "@2x.png");
+                future5El.appendChild(future5IconEl);
+
+                var dayFiveTemp = Math.round(data.daily[5].temp.day);
+                var dayFiveTempEl = document.createElement("span");
+                dayFiveTempEl.textContent = dayFiveTemp;
+                dayFiveTempForecastEl.appendChild(document.createTextNode(dayFiveTemp + "F"));
+
+                var dayFiveHumidity = data.daily[5].humidity;
+                var dayFiveHumidityEl = document.createElement("span");
+                dayFiveHumidityEl.textContent = dayFiveHumidity;
+                dayFiveHumidityForecastEl.appendChild(document.createTextNode(dayFiveHumidity + "%"));
+
+                var dayFiveWindSpeed = Math.round(data.daily[5].wind_speed);
+                var dayFiveWindSpeedEl = document.createElement("span");
+                dayFiveWindSpeedEl.textContent = dayFiveWindSpeed;
+                dayFiveWindForecastEl.appendChild(document.createTextNode(dayFiveWindSpeed + "MPH"));
+
+                var dailyDate5 = moment.unix(data.daily[5].dt).format("dd MM DD YYYY");
+                var dailyDate5El = document.createElement("span");
+                dailyDate5El.textContent = dailyDate5;
+                dayFiveDateEl.appendChild(document.createTextNode(dailyDate5));
 
             })
         }
